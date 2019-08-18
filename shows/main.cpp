e823248cfb7433d13af6c82b46819180762a4bbe
asm128 <pabloarielz@outlook.com>
2019-08-18 05:29:33 -0300

    + Unified redundancies under neutralizer's pageCatalog().

--- shows/main.cpp
+++ shows/main.cpp
@@ -36,20 +36,6 @@ GPK_CGI_JSON_APP_IMPL();
 		const ::gpk::error_t									indexRoot					= ::gpk::jsonExpressionResolve("obelisco", programState.Config.Reader, 0, rootNode);
 		::ntl::loadConfig(programState, indexRoot);
 	}
-	::gpk::array_pod<char_t>								fileStyle			;
-	::ntl::httpPath(programState.Path.Style		, "blankstyle"	, "css"	, fileStyle			);
-
-	output.append(::gpk::view_const_string{ "\n<html>"});
-	output.append(::gpk::view_const_string{ "\n<head>"});
-	output.append(::gpk::view_const_string{ "\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />"});
-	::ntl::htmlHeaderTitle		(programState.Page.Title, output);
-	::ntl::htmlHeaderStyleLink	({fileStyle.begin(), fileStyle.size()}, output);
-	output.append(::gpk::view_const_string{"\n</head>"});
-
-	output.append(::gpk::view_const_string{"\n<body style=\"width:100%;height:100%;background-color:#E0E0E0;font-family:Arial;\">"	}); // 202050
-	output.append(::gpk::view_const_string{ "\n<table style=\"width:100%;height:100%;text-align:center;\">"	});
-	output.append(::gpk::view_const_string{ "\n<tr style=\"\" >"});
-	output.append(::gpk::view_const_string{ "\n<td style=\"font-size:16px; font-weight:bold; vertical-align:top;\">"});
 	::gpk::view_const_string								lang;
 	::gpk::find("lang", requestReceived.QueryStringKeyVals, lang);
 	::gpk::view_const_string								title;
@@ -58,16 +44,7 @@ GPK_CGI_JSON_APP_IMPL();
 	else
 		title = (::gpk::view_const_string{ "Shows in Obelisco"});
 
-	::ntl::htmlBoardGenerate("ads.json", ::ntl::AD_SHOP_CATEGORY_shows, title, lang, output);
-
-	output.append(::gpk::view_const_string{"\n</td>"			});
-	output.append(::gpk::view_const_string{"\n</tr>"			});
-	output.append(::gpk::view_const_string{"\n</table>"			});
-	output.append(::gpk::view_const_string{ "\n<code >"	});
-	output.append(runtimeValues.Content.Body.begin(), runtimeValues.Content.Length);
-	output.append(::gpk::view_const_string{ "\n</code >"	});
-	output.append(::gpk::view_const_string{"\n</body>"			});
-	output.append(::gpk::view_const_string{"\n</html>"			});
+	::ntl::pageCatalog("ads.json", programState.Path.Style, ::ntl::AD_SHOP_CATEGORY_shows, title, lang, output);
 
 	if(output.size()) {
 		OutputDebugStringA(output.begin());
