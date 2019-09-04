000caf525b45d920586b14dc7a073e5e70f4f415
asm128 <pabloarielz@outlook.com>
2019-09-04 11:42:15 -0300

    + Implemented methods to retrieve urls and their components.

--- obervice/domainer.h
+++ obervice/domainer.h
@@ -22,12 +22,47 @@ namespace obe
 				::gpk::CViewManager<char_t, 1024*64>						Allocator					;
 
 				::gpk::array_pod<::obe::URL_SCHEME>							Scheme						;
-				::gpk::array_pod<int64_t>									Authority					;
-				::gpk::array_pod<int64_t>									Path						;
-				::gpk::array_pod<int64_t>									Query						;
-				::gpk::array_pod<int64_t>									Fragment					;
+				::gpk::array_pod<int32_t>									Authority					;
+				::gpk::array_pod<int32_t>									Path						;
+				::gpk::array_pod<int32_t>									Query						;
+				::gpk::array_pod<int32_t>									Fragment					;
 
 				::gpk::error_t												AddURLMap	(const ::gpk::view_const_char & textToAdd);
+
+				inline	::gpk::error_t										GetAuthority				(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Authority	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				inline	::gpk::error_t										GetPath						(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Path		[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				inline	::gpk::error_t										GetQuery					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Query		[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				inline	::gpk::error_t										GetFragment					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Fragment	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				::gpk::error_t												GetURL						(int32_t index, ::gpk::array_pod<char_t> & url)			{
+			const ::obe::URL_SCHEME													scheme						= Scheme	[index];
+			const int32_t															indexViewAuthority			= Authority	[index];
+			if(obe::URL_SCHEME_default != scheme) {
+				url.append(::gpk::get_value_label(scheme));
+				url.push_back(':');
+				if(-1 != indexViewAuthority) {
+					url.push_back('/');
+					url.push_back('/');
+				}
+			}
+			if(-1 != indexViewAuthority)
+				gpk_necall(url.append(Allocator.Views[indexViewAuthority], Allocator.Counts[indexViewAuthority]), "%s", "Out of memory?");
+			const int32_t															indexViewPath				= Path		[index];
+			if(-1 != indexViewPath) {
+				gpk_necall(url.push_back('/'), "%s", "Out of memory?");
+				gpk_necall(url.append(Allocator.Views[indexViewPath], Allocator.Counts[indexViewPath]), "%s", "Out of memory?");
+			}
+			const int32_t															indexViewQuery				= Query		[index];
+			if(-1 != indexViewQuery) {
+				gpk_necall(url.push_back('?'), "%s", "Out of memory?");
+				gpk_necall(url.append(Allocator.Views[indexViewQuery], Allocator.Counts[indexViewQuery]), "%s", "Out of memory?");
+			}
+			const int32_t															indexViewFragment			= Fragment	[index];
+			if(-1 != indexViewFragment) {
+				gpk_necall(url.push_back('#'), "%s", "Out of memory?");
+				gpk_necall(url.append(Allocator.Views[indexViewFragment], Allocator.Counts[indexViewFragment]), "%s", "Out of memory?");
+			}
+			return 0;
+		}
 	};
 
 	struct SSMTPMap			{
@@ -38,12 +73,12 @@ namespace obe
 
 				::gpk::error_t												AddSMTPMap					(const ::gpk::view_const_char & textToAdd);
 
-		inline	::gpk::error_t												GetDomain					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Domain	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex], }; return 0; }
-		inline	::gpk::error_t												GetUsername					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Username[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex], }; return 0; }
+		inline	::gpk::error_t												GetDomain					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Domain	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+		inline	::gpk::error_t												GetUsername					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Username[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
 				::gpk::error_t												GetEMail					(int32_t index, ::gpk::array_pod<char_t> & email)			{
 			const int32_t															indexViewDomain				= Domain	[index];
 			const int32_t															indexViewUsername			= Username	[index];
-			email																= ::gpk::view_const_char{Allocator.Views[indexViewDomain], Allocator.Counts[indexViewDomain]};
+			gpk_necall(email.append(Allocator.Views[indexViewDomain], Allocator.Counts[indexViewDomain]), "%s", "Out of memory?");
 			gpk_necall(email.push_back('@'), "%s", "Out of memory?");
 			gpk_necall(email.append(Allocator.Views[indexViewUsername], Allocator.Counts[indexViewUsername]), "%s", "Out of memory?");
 			return 0;
