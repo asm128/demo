75fd457774bad51956348ab8bd6fad137cf1de53
asm128 <pabloarielz@outlook.com>
2019-09-04 21:52:08 -0300

    + Added SSMTPMapBlock::Save() and SSMTPMapBlock::Load().

--- obervice/domainer.cpp
+++ obervice/domainer.cpp
@@ -3,19 +3,19 @@
 #include "gpk_stdstring.h"
 
 
-::gpk::error_t									obe::SSMTPMap::AddSMTPMap	(const ::gpk::view_const_char & textToAdd)		{
-	::gpk::error_t										indexToReturn				= -1;
+::gpk::error_t									obe::SSMTPMapBlock::AddSMTPMap		(const ::gpk::view_const_char & textToAdd)		{
+	::gpk::error_t										indexToReturn						= -1;
 
-	int32_t												idxUsername					= -1;
-	int32_t												idxDomain					= -1;
-	const ::gpk::error_t								arrobaPos					= ::gpk::find('@', textToAdd);
+	int32_t												idxUsername							= -1;
+	int32_t												idxDomain							= -1;
+	const ::gpk::error_t								arrobaPos							= ::gpk::find('@', textToAdd);
 	if(0 > arrobaPos) {
-		gpk_necall(idxUsername = Allocator.View(textToAdd.begin(), textToAdd.size()), "%s", "Out of memory?");
+		gpk_necall(idxUsername = Allocator.View(textToAdd.begin(), (uint16_t)textToAdd.size()), "%s", "Out of memory?");
 	}
 	else {
-		gpk_necall(idxUsername = Allocator.View(textToAdd.begin(), arrobaPos), "%s", "Out of memory?");
-		const uint32_t									offsetDomain				= arrobaPos + 1;
-		gpk_necall(idxDomain = Allocator.View(textToAdd.begin() + offsetDomain, textToAdd.size() - offsetDomain), "%s", "Out of memory?");
+		gpk_necall(idxUsername = Allocator.View(textToAdd.begin(), (uint16_t)arrobaPos), "%s", "Out of memory?");
+		const uint32_t									offsetDomain						= arrobaPos + 1;
+		gpk_necall(idxDomain = Allocator.View(textToAdd.begin() + offsetDomain, (uint16_t)textToAdd.size() - (uint16_t)offsetDomain), "%s", "Out of memory?");
 	}
 	gpk_necall(indexToReturn = Username	.push_back(idxUsername)	, "%s", "Out of memory?");
 	gpk_necall(indexToReturn = Domain	.push_back(idxDomain)	, "%s", "Out of memory?");
@@ -23,38 +23,38 @@
 	return indexToReturn;
 }
 
-::gpk::error_t									obe::SURLMap::AddURLMap		(const ::gpk::view_const_char & textToAdd)		{
-	::gpk::error_t										indexToReturn				= -1;
+::gpk::error_t									obe::SURLMapBlock::AddURLMap		(const ::gpk::view_const_char & textToAdd)		{
+	::gpk::error_t										indexToReturn						= -1;
 
-	int32_t												idxAuthority				= -1;
-	int32_t												idxPath						= -1;
-	int32_t												idxQuery					= -1;
-	int32_t												idxFragment					= -1;
+	int32_t												idxAuthority						= -1;
+	int32_t												idxPath								= -1;
+	int32_t												idxQuery							= -1;
+	int32_t												idxFragment							= -1;
 
 	// Read scheme
-	const ::gpk::error_t								colonpos					= ::gpk::find(':', textToAdd);
-	uint32_t											nextOffset					= 0;
+	const ::gpk::error_t								colonpos							= ::gpk::find(':', textToAdd);
+	uint32_t											nextOffset							= 0;
 	if(0 > colonpos)
 		gpk_necall(indexToReturn = Scheme.push_back(::obe::URL_SCHEME_default), "%s", "Out of memory?");
 	else {
 		nextOffset										= (colonpos + 1);
-		::gpk::array_pod<char_t>							scheme						= ::gpk::view_const_char{textToAdd.begin(), (uint32_t)colonpos};
+		::gpk::array_pod<char_t>							scheme								= ::gpk::view_const_char{textToAdd.begin(), (uint32_t)colonpos};
 		::gpk::tolower(scheme);
 		gpk_necall(indexToReturn = Scheme.push_back(::gpk::get_value<::obe::URL_SCHEME>(::gpk::view_const_string{scheme.begin(), scheme.size()})), "%s", "Out of memory?");
 	}
 	// Read either authority or path.
 	if(nextOffset < textToAdd.size()) {
-		::gpk::view_const_char								remainder					= ::gpk::view_const_char{textToAdd.begin() + nextOffset, textToAdd.size() - nextOffset};
+		::gpk::view_const_char								remainder							= ::gpk::view_const_char{textToAdd.begin() + nextOffset, textToAdd.size() - nextOffset};
 		if(remainder.size() > 1 && remainder[0] == '/' && remainder[1] == '/') { // Read optional authority component
 			remainder										= ::gpk::view_const_char{remainder.begin() + 2, remainder.size() - 2};
 			if(remainder.size()) {
-				const ::gpk::error_t								posPath						= ::gpk::find('/', remainder);
+				const ::gpk::error_t								posPath								= ::gpk::find('/', remainder);
 				if(0 > posPath) {
-					gpk_necall(idxAuthority = Allocator.View(remainder.begin(), remainder.size()), "%s", "Out of memory?");
+					gpk_necall(idxAuthority = Allocator.View(remainder.begin(), (uint16_t)remainder.size()), "%s", "Out of memory?");
 					remainder										= {};
 				}
 				else {
-					gpk_necall(idxAuthority = Allocator.View(remainder.begin(), posPath), "%s", "Out of memory?");
+					gpk_necall(idxAuthority = Allocator.View(remainder.begin(), (uint16_t)posPath), "%s", "Out of memory?");
 					remainder										= {remainder.begin() + posPath, remainder.size() - posPath};
 				}
 			}
@@ -62,19 +62,19 @@
 		if(remainder.size() && remainder[0] == '/') {	// Read Path component
 			remainder										= ::gpk::view_const_char{remainder.begin() + 1, remainder.size() - 1};
 			if(remainder.size()) {
-				const ::gpk::error_t								posQuery					= ::gpk::find('?', remainder);
+				const ::gpk::error_t								posQuery							= ::gpk::find('?', remainder);
 				if(0 <= posQuery) { // QueryString found. Read up to there.
-					gpk_necall(idxPath = Allocator.View(remainder.begin(), posQuery), "%s", "Out of memory?");
+					gpk_necall(idxPath = Allocator.View(remainder.begin(), (uint16_t)posQuery), "%s", "Out of memory?");
 					remainder										= ::gpk::view_const_char{remainder.begin() + posQuery, remainder.size() - posQuery};
 				}
 				else { // No QueryString found. Read until the end unless fragment is found.
-					const ::gpk::error_t								posFragment					= ::gpk::find('#', ::gpk::view_const_char{&remainder[posQuery], remainder.size() - posQuery});
+					const ::gpk::error_t								posFragment							= ::gpk::find('#', ::gpk::view_const_char{&remainder[posQuery], remainder.size() - posQuery});
 					if(0 > posFragment) { // Read until the end unless fragment is found.
-						gpk_necall(idxPath = Allocator.View(remainder.begin(), remainder.size()), "%s", "Out of memory?");
+						gpk_necall(idxPath = Allocator.View(remainder.begin(), (uint16_t)remainder.size()), "%s", "Out of memory?");
 						remainder										= {};
 					}
 					else {
-						gpk_necall(idxPath = Allocator.View(remainder.begin(), posFragment), "%s", "Out of memory?");
+						gpk_necall(idxPath = Allocator.View(remainder.begin(), (uint16_t)posFragment), "%s", "Out of memory?");
 						remainder										= ::gpk::view_const_char{remainder.begin() + posFragment, remainder.size() - posFragment};
 					}
 				}
@@ -85,11 +85,11 @@
 			if(remainder.size()) {
 				const ::gpk::error_t								posFragment						= ::gpk::find('#', remainder);
 				if(0 > posFragment) { // Read until the end unless fragment is found.
-					gpk_necall(idxPath = Allocator.View(remainder.begin(), remainder.size()), "%s", "Out of memory?");
+					gpk_necall(idxPath = Allocator.View(remainder.begin(), (uint16_t)remainder.size()), "%s", "Out of memory?");
 					remainder										= {};
 				}
 				else {
-					gpk_necall(idxPath = Allocator.View(remainder.begin(), posFragment), "%s", "Out of memory?");
+					gpk_necall(idxPath = Allocator.View(remainder.begin(), (uint16_t)posFragment), "%s", "Out of memory?");
 					remainder										= ::gpk::view_const_char{remainder.begin() + posFragment, remainder.size() - posFragment};
 				}
 			}
@@ -97,7 +97,7 @@
 		if(remainder.size() && remainder[0] == '#') {	// Read fragment component
 			remainder										= ::gpk::view_const_char{remainder.begin() + 1, remainder.size() - 1};
 			if(remainder.size())
-				gpk_necall(idxFragment = Allocator.View(remainder.begin(), remainder.size()), "%s", "Out of memory?");
+				gpk_necall(idxFragment = Allocator.View(remainder.begin(), (uint16_t)remainder.size()), "%s", "Out of memory?");
 		}
 	}
 	gpk_necall(Authority.push_back(idxAuthority	), "%s", "Out of memory?");
--- obervice/domainer.h
+++ obervice/domainer.h
@@ -18,8 +18,8 @@ namespace obe
 	GDEFINE_ENUM_VALUE(URL_SCHEME, data		, 6);
 	GDEFINE_ENUM_VALUE(URL_SCHEME, irc		, 7);
 
-	struct SURLMap			{
-				::gpk::CViewManager<char_t, 1024*64>						Allocator					;
+	struct SURLMapBlock			{
+				::gpk::CViewManager<char_t, 1023U*64U>						Allocator					;
 
 				::gpk::array_pod<::obe::URL_SCHEME>							Scheme						;
 				::gpk::array_pod<int32_t>									Authority					;
@@ -27,16 +27,16 @@ namespace obe
 				::gpk::array_pod<int32_t>									Query						;
 				::gpk::array_pod<int32_t>									Fragment					;
 
-				::gpk::error_t												AddURLMap	(const ::gpk::view_const_char & textToAdd);
+				::gpk::error_t												AddURLMap					(const ::gpk::view_const_char & textToAdd);
 
-				inline	::gpk::error_t										GetAuthority				(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Authority	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
-				inline	::gpk::error_t										GetPath						(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Path		[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
-				inline	::gpk::error_t										GetQuery					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Query		[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
-				inline	::gpk::error_t										GetFragment					(int32_t index, ::gpk::view_const_char & output)			{ const int32_t viewIndex = Fragment	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
-				::gpk::error_t												GetURL						(int32_t index, ::gpk::array_pod<char_t> & url)			{
+				inline	::gpk::error_t										GetAuthority				(int32_t index, ::gpk::view_const_char & output)	{ const int32_t viewIndex = Authority	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				inline	::gpk::error_t										GetPath						(int32_t index, ::gpk::view_const_char & output)	{ const int32_t viewIndex = Path		[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				inline	::gpk::error_t										GetQuery					(int32_t index, ::gpk::view_const_char & output)	{ const int32_t viewIndex = Query		[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				inline	::gpk::error_t										GetFragment					(int32_t index, ::gpk::view_const_char & output)	{ const int32_t viewIndex = Fragment	[index]; output = {Allocator.Views[viewIndex], Allocator.Counts[viewIndex]}; return 0; }
+				::gpk::error_t												GetURL						(int32_t index, ::gpk::array_pod<char_t> & url)		{
 			const ::obe::URL_SCHEME													scheme						= Scheme	[index];
 			const int32_t															indexViewAuthority			= Authority	[index];
-			if(obe::URL_SCHEME_default != scheme) {
+			if(::obe::URL_SCHEME_default != scheme) {
 				url.append(::gpk::get_value_label(scheme));
 				url.push_back(':');
 				if(-1 != indexViewAuthority) {
@@ -65,11 +65,48 @@ namespace obe
 		}
 	};
 
-	struct SSMTPMap			{
+	struct SSMTPMapBlock			{
 				::gpk::CViewManager<char_t, 1024*64>						Allocator					;
 
 				::gpk::array_pod<int32_t>									Domain						;
-				::gpk::array_obj<int32_t>									Username					;
+				::gpk::array_pod<int32_t>									Username					;
+
+				::gpk::error_t												Save						(::gpk::array_pod<byte_t> & output)	{
+			::gpk::viewWrite(::gpk::view_const_uint16{Allocator.Counts.begin(), Allocator.Counts.size()}, output);
+			for(uint32_t iArray = 0; iArray < Allocator.Counts.size(); ++iArray)
+				output.append(Allocator.Views[iArray], Allocator.Counts[iArray]);
+
+			::gpk::viewWrite(::gpk::view_const_int32{Domain.begin(), Domain.size()}, output);
+			output.append(::gpk::view_const_byte{(const byte_t*)Username.begin(), Domain.size() * sizeof(int32_t)});
+		}
+
+				::gpk::error_t												Load						(const ::gpk::view_const_byte & input)	{
+			if(0 == input.size())
+				return 0;
+
+			const uint32_t															countArrays					= *(const uint32_t*)input.begin();
+			uint32_t																offsetArraySize				= sizeof(uint32_t);
+			typedef																	uint16_t					_tViewLen;
+			uint32_t																offsetArrayData				= offsetArraySize + sizeof(_tViewLen) * countArrays;
+			for(uint32_t iArray = 0; iArray < countArrays; ++iArray) {
+				const _tViewLen															currentArraySize			= *(_tViewLen*)&input[offsetArraySize];
+				Allocator.View(&input[offsetArrayData], currentArraySize);
+				offsetArrayData														+= currentArraySize;
+				offsetArraySize														+= sizeof(_tViewLen);
+			}
+			const uint32_t															countMaps					= *(const uint32_t*)&input[offsetArrayData];
+			uint32_t																offsetDataDomain			= offsetArrayData	+ sizeof(uint32_t);
+			uint32_t																offsetDataUsername			= offsetDataDomain	+ sizeof(int32_t) * countMaps;
+			gpk_necall(Domain	.resize(countMaps), "%s", "Out of memory?");
+			gpk_necall(Username	.resize(countMaps), "%s", "Out of memory?");
+			for(uint32_t iMap = 0; iMap < countArrays; ++iMap) {
+				Domain	[iMap]														= *(int32_t*)&input[offsetDataDomain	];
+				Username[iMap]														= *(int32_t*)&input[offsetDataUsername	];
+				offsetArrayData														+= sizeof(int32_t);
+				offsetArraySize														+= sizeof(int32_t);
+			}
+			return 0;
+		}
 
 				::gpk::error_t												AddSMTPMap					(const ::gpk::view_const_char & textToAdd);
 
